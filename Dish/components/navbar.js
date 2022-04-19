e40a523ab88df00be4dedf138fb80a104f466397
MD Noorish Nauman <97448658+Noorish100@users.noreply.github.com>
2022-04-19 14:43:17 +0530

    done

---
+++ Dish/components/navbar.js
@@ -0,0 +1,41 @@
+function navbar()
+{
+ 
+  return   ` <div class="left"></div>
+        <div class="right">
+      <div onclick="window.location.href='index.html'"> Menu</div>
+      <div onclick="window.location.href='latest.html'">Latest Receipe</div>
+      <div onclick="window.location.href='today.html'"> Receipe Of The Day</div>
+  </div>
+  </div>`
+  
+ 
+
+}
+
+export default navbar;
+
+
+
+
+// function navbar(){
+
+
+//   return ` <div id="navbar">
+//   <div>
+//     <h3> <a href="/">Home</a> </h3>
+//   </div>
+  
+//   <div class="options">
+//     <h3><a href="jewellery.html">Jewellery</a></h3>
+//     <h3><a href="electronics.html">Electronics</a></h3>
+//     <h3><a href="#">Register</a></h3>
+//     <h3><a href="#">Login</a></h3>
+  
+//   </div>
+//   </div>`
+  
+//   }
+  
+//   export default navbar;
+  
